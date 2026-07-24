9e82e160b965a312097dd6fc71cb6526e04edde6
keshavsoft <nknnkumar@yahoo.com>
2026-07-24 23:28:43 +0530

    to test

---
+++ test/v7/test.js
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const appJsPath = path.join(__dirname, "routes.js");
+
+import defaultFunc from '../../index.js';
+
+import extractRegex from './extractRegex.js';
+
+const fileContent = fs.readFileSync(appJsPath, 'utf8');
+
+const k1 = defaultFunc({
+    fileContent,
+    extractRegex,
+    showLog: false,
+    showLogStep1: false,
+    showLogStep2: false
+});
+
+console.log("ssssssssss : ", k1);
+
